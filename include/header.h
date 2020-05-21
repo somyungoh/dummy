d7cc30c1d37b0228fb6ae7dc89e68f6767cc41cf
Somyung Oh <somyungoh@gmail.com>
2020-05-20 21:26:08 -0500

    initial commit

---
+++ include/header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct covid {
+
+    int cases;
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "include/header.h"
+#include <cstdio>
+
+int main (int argc, char** argv) {
+
+    printf("Howdy!\n");
+
+    return 0;
+}
